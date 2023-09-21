51de0955a551bda88dcb1ea6ba8cbb1a419f01e7
ZeroMeia123 <silvadan752@gmail.com>
2023-09-20 21:42:40 -0300

    upando testes com o site sauceDemo

---
+++ cypress/e2e/sauceDemo/sauceDemo.js
@@ -0,0 +1,13 @@
+import {Given, When, Then} from "@badeball/cypress-cucumber-preprocessor";
+Given("eu entro no site",()=>{
+    cy.visit('https://www.saucedemo.com/');
+})
+When("eu escrevo {string} no Username",(login)=>{
+    cy.get('[data-test="username"]').type(login);
+})
+When("eu escrevo {string} no Password e dou enter",(senha)=>{
+    cy.get('[data-test="password"]').type(`${senha}{enter}`);
+})
+Then("deve ser visto {string} na tela",(palavra)=>{
+    cy.contains(palavra);
+})
